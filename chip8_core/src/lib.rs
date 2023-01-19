366272cdec4372b164653eca1b2dce5e17f675cd
Lucas Rocha <lucas.rocha.3801@gmail.com>
2023-01-18 23:49:13 -0300

    muita coisa =D

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,5 +1,29 @@
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+];
+
+
+
+
 pub const SCREEN_WIDTH: usize = 64;
-pub const SCEEN_HEIGHT: usize = 32;
+pub const SCREEN_HEIGHT: usize = 32;
 
 const RAM_SIZE: usize = 4096;
 const NUM_REGS: usize = 16;
@@ -10,7 +34,7 @@ const START_ADDR: u16 = 0x200;
 pub struct Emu { //main object for the emulator backend
     pc: u16,
     ram: [u8; RAM_SIZE],
-    screen: [bool; SCREEN_WIDTH * SCEEN_HEIGHT],
+    screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
     v_regs: [u8; NUM_REGS],
     i_reg: u16,
     sp: u16,
@@ -22,10 +46,10 @@ pub struct Emu { //main object for the emulator backend
 
 impl Emu {
     pub fn new() -> Self {
-        Self {
+        let mut new_emu  = Self {
             pc: START_ADDR,
             ram: [0; RAM_SIZE],
-            screen: [false; SCEEN_HEIGHT * SCREEN_WIDTH],
+            screen: [false; SCREEN_HEIGHT * SCREEN_WIDTH],
             v_regs: [0; NUM_REGS],
             i_reg: 0,
             sp: 0,
@@ -33,6 +57,87 @@ impl Emu {
             keys: [false; NUM_KEYS],
             dt: 0,
             st: 0,
+        };
+
+        new_emu.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        new_emu
+    }
+
+    pub fn reset(&mut self) {
+        self.pc = START_ADDR;
+        self.ram = [0;RAM_SIZE];
+        self.screen = [false; SCREEN_HEIGHT*SCREEN_WIDTH];
+        self.v_regs = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0;STACK_SIZE];
+        self.keys = [false; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.ram[..FONTSET_SIZE].copy_from_slice(&FONTSET)
+    }
+
+    fn push(&mut self, val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    fn pop(&mut self) -> u16{
+        self.sp -= 1;
+        self.stack[self.sp as usize]
+    }
+
+    pub fn tick(&mut self) {
+        let op = self.fetch();
+
+        self.execute(op);
+    }
+
+    pub fn tick_timers(&mut self) {
+        if self.dt > 0 {
+            self.dt -= 1;
         }
+
+        if self.st > 0 {
+            if self.st == 1 {
+                //BEEP
+            }
+            self.st -= 1;
+        }
+    }
+
+    fn execute(&mut self, op: u16) {
+        let digit1 = (op & 0xF000) >> 12;
+        let digit2 = (op & 0x0F00) >> 8;
+        let digit3 = (op & 0x00F0) >> 4;
+        let digit4 = op & 0x000F;
+
+        match (digit1, digit2, digit3, digit4) {
+            (_, _, _, _) => unimplemented!("Unimplemented opcode: {}",op)
+        }
+    }
+    
+    fn fetch(&mut self) -> u16 {
+        let higher_byte = self.ram[self.pc as usize] as u16;
+        let lower_byte = self.ram[(self.pc+1) as usize] as u16;
+        let op = (higher_byte << 8) | lower_byte;
+        self.pc += 2;
+        op
+    }
+
+    // FRONTEND
+    pub fn get_display(&self) -> &[bool] {
+        &self.screen
+    }
+
+    pub fn keypress(&mut self, idx: usize, pressed: bool) {
+        self.keys[idx] = pressed;
+    }
+
+    pub fn load(&mut self, data: &[u8]) {
+        let start = START_ADDR as usize;
+        let end = (START_ADDR as usize) + data.len();
+        self.ram[start..end].copy_from_slice(data);
     }
 }
