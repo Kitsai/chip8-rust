193d81d79cb662e1ff104a287ed82a81ae9b52bb
Lucas Rocha <102306509+Kitsai@users.noreply.github.com>
2023-04-25 22:24:06 -0300

    asdads

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,10 +1,11 @@
-use std::env;
 use chip8_core::*;
+use std::env;
 
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -12,7 +13,6 @@ fn main() {
         println!("Usage: cargo run path/to/game");
         return;
     }
-
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
@@ -24,5 +24,4 @@ fn main() {
     let mut canvas = window.into_canvas().present_vsync().build().unwrap();
     canvas.clear();
     canvas.present();
-
 }
